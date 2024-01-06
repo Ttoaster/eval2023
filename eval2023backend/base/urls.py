4c4e309fade7799cd9f8e0449762363d30db4644
RLowe <richard950pt@gmail.com>
2024-01-06 12:29:39 -0500

    clean up

--- eval2023backend/base/urls.py
+++ eval2023backend/base/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 from .views import create_wallet, get_balance, send_money, fund_wallet, get_wallets, get_wallet_details, delete_user_wallet, get_data, get_transaction
-from . import views
 
 
 
 
 urlpatterns = [
-    # path('', views.index),
-    # path('add/', views.add_person),
-    # path('show/', views.get_all_person),
     path('create_wallet/', create_wallet, name='create_wallet'),
     path('create_wallet/<str:newWalletName>', create_wallet, name='create_wallet'),
     path('delete_user_wallet/', delete_user_wallet, name='delete_user_wallet'),
